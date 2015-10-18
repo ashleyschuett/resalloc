64634fd4239e6f9e5f6ee4e4aceb17bf8c773f91
Michael Schuett <michaeljs1990@gmail.com>
2015-10-18 19:14:10 -0400

    Add models and controllers
    Implimentation for all of the endpoints as well as the data access layer.

--- controllers.go
+++ controllers.go
@@ -2,21 +2,488 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/julienschmidt/httprouter"
+	"github.com/samalba/dockerclient"
 )
+
+// Register contains all information that should
+// be passed into a valid POST /register call
+type Register struct {
+	Name     string `valid:"alphanum,required"`
+	Password string `valid:"alphanum,required"`
+}
+
+// RegisterSuccess is used upon a successful
+// POST /register call
+type RegisterSuccess struct {
+	Success bool
+	Message string
+}
 
 // RegisterController handles account creation
 func RegisterController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var s RegisterModel
-	decoder := json.NewDecoder(r.Body)
-	decoder.Decode(&s)
-	fmt.Println(s)
+	var s Register
+	var sr = RegisterSuccess{false, ""}
+
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	// Result ignored since a nil err value tells us
+	// the same thing that a result of true does.
+	if _, err := govalidator.ValidateStruct(s); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	if err := (User{
+		Username: s.Name,
+		Password: s.Password,
+	}.Create()); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Message = "Users has been created"
+	sr.Success = true
+	w.Write(Marshal(sr))
+	return
+}
+
+// Login although the same as the Register struct using the same
+// struct for both did not feel right and makes the code
+// slightly confusing.
+type Login struct {
+	Name     string `valid:"alphanum,required"`
+	Password string `valid:"alphanum,required"`
+}
+
+// LoginSuccess is used upon a successful
+// POST /login call
+type LoginSuccess struct {
+	Success bool
+	Message string
+	Token   string
 }
 
 // LoginController handles authentication and token generation
 func LoginController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var s Login
+	var sr = LoginSuccess{false, "", ""}
 
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	_, err := govalidator.ValidateStruct(s)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	user := User{}
+	if err := (user.Polulate(s.Name)); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	if err := validPassword(s.Password, user.Password); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	if err := user.GenerateToken(); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Token = user.Token
+	sr.Success = true
+	w.Write(Marshal(sr))
+	return
+}
+
+// ListResourceSuccess is used upon a successful
+// GET /resource call
+type ListResourceSuccess struct {
+	Success   bool
+	Message   string
+	Resources []Resource
+}
+
+// ListResourceController brings back a list of all resources
+// that are current available to that user
+func ListResourceController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	sr := ListResourceSuccess{false, "", nil}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	resources, err := Resource{}.FetchAll()
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Success = true
+	sr.Resources = resources
+	w.Write(Marshal(sr))
+	return
+}
+
+// CreateResource validates incomming requests
+type CreateResource struct {
+	Name string `valid:"alphanum,required"`
+	File string `valid:"required"`
+}
+
+// CreateResourceSuccess ...
+type CreateResourceSuccess struct {
+	Success bool
+	Message string
+}
+
+// CreateResourceController creates a new resource that all users now have access to
+func CreateResourceController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var s CreateResource
+	sr := CreateResourceSuccess{false, ""}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	_, err := govalidator.ValidateStruct(s)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	if err := (Resource{
+		Name: s.Name,
+		File: s.File,
+	}.Create()); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Success = true
+	sr.Message = "Your resource is now available for use in new containers"
+	w.Write(Marshal(sr))
+	return
+}
+
+// CreateMachine validates that the input is valid
+// to an extent... Validation could be better for
+// this controller.
+type CreateMachine struct {
+	Name     string `valid:"alphanum,required"`
+	Username string `valid:"alphanum,required"`
+	IP       string `valid:"required"`
+}
+
+// CreateMachineSuccess is returned
+// when the controller has successfully
+// returned
+type CreateMachineSuccess struct {
+	Success bool
+	Message string
+}
+
+// CreateMachineController adds a new machine that can be used to launch containers on
+func CreateMachineController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var s CreateMachine
+	sr := CreateMachineSuccess{false, ""}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	_, err := govalidator.ValidateStruct(s)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	if err := (Machine{
+		Name:     s.Name,
+		Username: s.Username,
+		IP:       s.IP,
+	}.Create()); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Success = true
+	sr.Message = "Your machine has been added to the pool of machines"
+	w.Write(Marshal(sr))
+	return
+}
+
+// CreateLease validates that a resource name
+// Has been specified
+type CreateLease struct {
+	ResourceName string `valid:"alphanum,required"` // As set in the resources table
+	LeaseName    string `valid:"alphanum,required"` // name of container on remote machine
+}
+
+// CreateLeaseSuccess ...
+type CreateLeaseSuccess struct {
+	Success  bool
+	Message  string
+	Username string
+	Machine  string
+	Name     string
+}
+
+// CreateLeaseController creates a new machine for a user to use
+func CreateLeaseController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var s CreateLease
+	sr := CreateLeaseSuccess{false, "", "", "", ""}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	_, err := govalidator.ValidateStruct(s)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Fetch machine to deploy to
+	m := Machine{}
+	if err := m.FetchRand(); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Fetch type of resource to build
+	res := Resource{}
+	if err := res.Fetch(s.ResourceName); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Create a tar file in memory that can be used
+	// to send to docker to create a new image without
+	// ever writing to disk
+	tar, err := MakeTarFile(res.File)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Connect to remote machine
+	docker, err := dockerclient.NewDockerClient("http://"+m.IP+":5555", nil)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Build a docker image on remote machine
+	buildImageConfig := &dockerclient.BuildImage{
+		Context:        tar,
+		RepoName:       res.Name,
+		SuppressOutput: false,
+	}
+	// Try to create image on remote docker machine
+	reader, err := docker.BuildImage(buildImageConfig)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// If called before checking for an error and an error occurs you will
+	// get a null pointer dereferenced error.
+	defer reader.Close()
+	// These two commands are super helpful but it is returned as
+	// a stream of json... In a better implementation we would want to
+	// send this back to the client as it comes in. For now I have just
+	// left it in to help with debugging.
+	// text, _ := ioutil.ReadAll(reader)
+	// fmt.Println(string(text))
+	// Create a container
+	containerConfig := &dockerclient.ContainerConfig{
+		Image:       res.Name,
+		Cmd:         []string{"ping", "8.8.8.8"},
+		AttachStdin: true,
+		Tty:         true}
+	containerID, err := docker.CreateContainer(containerConfig, s.LeaseName)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	// Start the container on randomly chosen machine
+	hostConfig := &dockerclient.HostConfig{}
+	// For some reason this API returns the full 64 character hex String
+	// which can't be used when starting the container as it only takes the
+	// first 12 characters. Possibly a bad api or exists for legacy reasons.
+	err = docker.StartContainer(containerID[0:12], hostConfig)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	// Save Lease in database
+	Lease{
+		Name:        s.LeaseName,
+		Username:    m.Username,
+		MachineName: m.IP,
+	}.Create()
+
+	sr.Success = true
+	sr.Message = "Your lease was successfully fulfilled"
+	sr.Machine = m.IP
+	sr.Username = m.Username
+	sr.Name = s.LeaseName
+	w.Write(Marshal(sr))
+	return
+}
+
+// ListLeaseSuccess ...
+type ListLeaseSuccess struct {
+	Success bool
+	Message string
+	Leases  []Lease
+}
+
+// ListLeasesController returns all leases for a specific client
+func ListLeasesController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	sr := ListLeaseSuccess{false, "", nil}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	leases, err := Lease{}.FetchAll()
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	sr.Success = true
+	sr.Leases = leases
+	w.Write(Marshal(sr))
+	return
+}
+
+// DeleteLease requires the name of the
+// container so that it can be deleted
+type DeleteLease struct {
+	Name string `valid:"alphanum,required"`
+}
+
+// DeleteLeaseSuccess ...
+type DeleteLeaseSuccess struct {
+	Success bool
+	Message string
+}
+
+// DeleteLeaseController removes a lease from the remote machine
+func DeleteLeaseController(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var s DeleteLease
+	sr := DeleteLeaseSuccess{}
+
+	if err := VerifyToken(r.Header.Get("token")); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(401)
+		w.Write(Marshal(sr))
+		return
+	}
+
+	json.NewDecoder(r.Body).Decode(&s)
+	r.Body.Close()
+	_, err := govalidator.ValidateStruct(s)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Get information about lease so we know what machine it is located on
+	l := Lease{}
+	if err := l.Fetch(s.Name); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Connect to machine that the lease is on
+	docker, err := dockerclient.NewDockerClient("http://"+l.MachineName+":5555", nil)
+	if err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Remove the container
+	if err := docker.RemoveContainer(s.Name, true, true); err != nil {
+		sr.Message = err.Error()
+		w.WriteHeader(400)
+		w.Write(Marshal(sr))
+		return
+	}
+	// Remove container from the database
+	l.Delete()
+
+	sr.Success = true
+	sr.Message = "Your lease has been removed"
+	w.Write(Marshal(sr))
+	return
 }
--- models.go
+++ models.go
@@ -1,5 +1,260 @@
 package main
 
-type RegisterModel struct {
-	Name, Password string
+import (
+	"database/sql"
+	"errors"
+	"log"
+
+	"github.com/doug-martin/goqu"
+	"github.com/jmcvetta/randutil"
+	_ "github.com/mattn/go-sqlite3" // sqlite3 adapter
+)
+
+var db = setDB()
+
+func setDB() *goqu.Database {
+	sqliteDb, err := sql.Open("sqlite3", "./resalloc.db")
+	if err != nil {
+		log.Print(err.Error())
+	}
+	db := goqu.New("sqlite3", sqliteDb)
+	return db
+}
+
+// User represents a row from
+// the users table
+type User struct {
+	Username    string `db:"username"`
+	Password    string `db:"password"`
+	AccountType string `db:"account_type"`
+	Activated   int    `db:"activated"`
+	Token       string `db:"token"`
+}
+
+// Create inserts one row
+// into the database
+func (u User) Create() error {
+	// Generate hashed password and store to db
+	u.Password = generatePassword(u.Password)
+	if _, err := db.From("users").
+		Insert(goqu.Record{
+		"username":     u.Username,
+		"password":     u.Password,
+		"token":        "",
+		"account_type": "USR",
+		"activated":    1,
+	}).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Polulate takes a username and populates
+// a User struct with information from the db
+func (u *User) Polulate(username string) error {
+	if _, err := db.From("users").
+		Where(goqu.Ex{
+		"username": username,
+	}).ScanStruct(u); err != nil {
+		return err
+	}
+	return nil
+}
+
+// PopulateFromToken populates the user struct
+// when only a token is available
+func (u *User) PopulateFromToken(token string) error {
+	if _, err := db.From("users").
+		Where(goqu.Ex{
+		"token": token,
+	}).ScanStruct(u); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GenerateToken creates a sudo random string
+// and stores it in the database
+func (u *User) GenerateToken() error {
+	u.Token, _ = randutil.String(50, "7148fa22885551edb6405895ddf7cd0f")
+	if _, err := db.From("users").
+		Where(goqu.Ex{
+		"username": u.Username,
+	}).Update(goqu.Record{
+		"token": u.Token,
+	}).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// VerifyToken ensures that the passed in
+// token is a valid token in the database
+func VerifyToken(token string) error {
+	var count int
+	if _, err := db.From("users").
+		Select(goqu.COUNT("*")).
+		Where(goqu.Ex{
+		"token": token,
+	}).ScanVal(&count); err != nil {
+		return err
+	}
+
+	if count != 1 {
+		return errors.New("No valid token found")
+	}
+
+	return nil
+}
+
+// Resource represents the structure
+// of the resources table in the database
+type Resource struct {
+	Name string `db:"name"`
+	File string `db:"file"`
+}
+
+// Create inserts a new row into the
+// resources table
+func (r Resource) Create() error {
+	// Generate hashed password and store to db
+	if _, err := db.From("resources").
+		Insert(goqu.Record{
+		"name": r.Name,
+		"file": r.File,
+	}).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Fetch grabs one machine for creation
+func (r *Resource) Fetch(name string) error {
+	if _, err := db.From("resources").
+		Where(goqu.Ex{
+		"name": name,
+	}).ScanStruct(r); err != nil {
+		return err
+	}
+	return nil
+}
+
+// FetchAll gets a list of all available resources
+// that are in the database this is kind of odd
+// since it is actually not requires that you have
+// already instantiated a Resource struct to make this
+// call... however I find from a readablity standpoint
+// it makes more sense if you call this on an empty
+// Resource struct. Renaming it FetchResouces would
+// be another option.
+func (r Resource) FetchAll() ([]Resource, error) {
+	var resources []Resource
+	if err := db.From("resources").
+		ScanStructs(&resources); err != nil {
+		return nil, err
+	}
+	return resources, nil
+}
+
+// Machine represents the structure
+// of the resources table in the database
+type Machine struct {
+	Name     string `db:"name"`
+	Username string `db:"username"`
+	IP       string `db:"ip"`
+}
+
+// Create inserts a new row into the
+// resources table
+func (m Machine) Create() error {
+	// Generate hashed password and store to db
+	if _, err := db.From("machines").
+		Insert(goqu.Record{
+		"name":     m.Name,
+		"username": m.Username,
+		"ip":       m.IP,
+	}).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// FetchRand will grab a random machine
+// to spin up the requests lease on
+// In reality images should likely have tags
+// so that you can better optimize how the
+// hardware would be shared among a large
+// amount of users
+func (m *Machine) FetchRand() error {
+	// Goqu doesn't support order by random
+	// So i'm faking it in order to take
+	// advantage of the ScanStructs function still
+	var ms []Machine
+	if err := db.From("machines").
+		ScanStructs(&ms); err != nil {
+		return err
+	}
+	// Get "random" row
+	total := len(ms)
+	randInt, _ := randutil.IntRange(0, 1000)
+	row := randInt % total
+	*m = ms[row]
+	return nil
+}
+
+// Lease represents a lease that has been
+// created on a remote machine
+type Lease struct {
+	Name        string `db:"name"`
+	Username    string `db:"username"`
+	MachineName string `db:"machine_name"`
+}
+
+// Create inserts a new row into the
+// resources table
+func (l Lease) Create() error {
+	// Generate hashed password and store to db
+	if _, err := db.From("leases").
+		Insert(goqu.Record{
+		"name":         l.Name,
+		"username":     l.Username,
+		"machine_name": l.MachineName,
+	}).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// FetchAll returns a list of all active leases
+func (l Lease) FetchAll() ([]Lease, error) {
+	var leases []Lease
+	if err := db.From("leases").
+		ScanStructs(&leases); err != nil {
+		return nil, err
+	}
+	return leases, nil
+}
+
+// Fetch returns a lease if it exists
+func (l *Lease) Fetch(name string) error {
+	if _, err := db.From("leases").
+		Where(goqu.Ex{
+		"name": name,
+	}).ScanStruct(l); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete will remove the current
+// value stored in l.Name from the
+// database.
+func (l *Lease) Delete() error {
+	if _, err := db.From("leases").
+		Where(goqu.Ex{
+		"name": l.Name,
+	}).Delete().Exec(); err != nil {
+		return err
+	}
+	return nil
 }
